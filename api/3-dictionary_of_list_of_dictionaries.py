79945b2017f040c92c03d96e15f217358fabd0ee
Gotmished <5002@holbertonstudents.com>
2022-12-04 10:55:10 +0000

    Initial commit task three - create a dictionary containing dictionaries of each user task set

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module to gather data from an API, and export in JSON format:
+https://jsonplaceholder.typicode.com/
+"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+def data_retrieval_export():
+    """Retrieving data from an API for export in JSON"""
+    # Setting variable for users and to-dos with url of API
+    users_list = 'https://jsonplaceholder.typicode.com/users/'
+    task_list = 'https://jsonplaceholder.typicode.com/todos'
+
+    # Requesting information, and converting to json format
+    users = requests.get(users_list).json()
+    tasks = requests.get(task_list).json()
+    details_dict = {}
+
+    # For each user, find all tasks by iteration, and create a dictionary
+    for each_user in users:
+        user_tasks = []
+        for each_task in tasks:
+            # Order is strange - get() may be a better option in the future
+            order_of_details = {"task": each_task['title'],
+                                "completed": each_task['completed'],
+                                "username": each_user['username']}
+            user_tasks.append(order_of_details)
+        id_key = each_user['id']
+        # In the dictionary, for key=particular ID, associate tasks
+        details_dict[id_key] = user_tasks
+
+    with open('todo_all_employees.json', 'w', encoding='utf-8') as file:
+        json.dump(details_dict, file)
+
+
+# Accepting user input as ID
+if __name__ == '__main__':
+    data_retrieval_export()
